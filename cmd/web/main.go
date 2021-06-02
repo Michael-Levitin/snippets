65b0e879dad0eee0fdfe5fa7c9f9ca5d545b165a
Michael Levitin <michael9levi@gmail.com>
2021-06-02 15:44:01 +0300

    Rewrited logging for HTTP-sever with log.New Temporary deleted check for dirs without index.html

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,14 +4,14 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"path/filepath"
+	"os"
 )
 
 func main() {
 	// Создаем новый флаг командной строки, значение по умолчанию: ":4000".
 	// Добавляем небольшую справку, объясняющая, что содержит данный флаг.
 	// Значение флага будет сохранено в переменной addr.
-	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
+	addr := flag.String("addr", ":4000", "Сетевой адрес веб-сервера")
 
 	// Мы вызываем функцию flag.Parse() для извлечения флага из командной строки.
 	// Она считывает значение флага из командной строки и присваивает его содержимое
@@ -19,6 +19,18 @@ func main() {
 	// иначе она всегда будет содержать значение по умолчанию ":4000".
 	// Если есть ошибки во время извлечения данных - приложение будет остановлено.
 	flag.Parse()
+
+	// Используйте log.New() для создания логгера для записи информационных сообщений. Для этого нужно
+	// три параметра: место назначения для записи логов (os.Stdout), строка
+	// с префиксом сообщения (INFO или ERROR) и флаги, указывающие, какая
+	// дополнительная информация будет добавлена. Обратите внимание, что флаги
+	// соединяются с помощью оператора OR |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Создаем логгер для записи сообщений об ошибках таким же образом, но используем stderr как
+	// место для записи и используем флаг log.Lshortfile для включения в лог
+	// названия файла и номера строки где обнаружилась ошибка.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// Используется функция http.NewServeMux() для инициализации нового рутера, затем
 	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
@@ -30,9 +42,18 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 	// HandleFunc() - адаптер для превращения функции в обработчик
 	// - добавляет медот ServeHTTP() в функцию
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
-	mux.Handle("/static", http.NotFoundHandler())
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Инициализируем новую структуру http.Server. Мы устанавливаем поля Addr и Handler, так
+	// что сервер использует тот же сетевой адрес и маршруты, что и раньше, и назначаем
+	// поле ErrorLog, чтобы сервер использовал наш логгер
+	// при возникновении проблем.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
 
 	// Используется функция http.ListenAndServe() для запуска нового веб-сервера.
 	// Мы передаем два параметра: TCP-адрес сети для прослушивания (в данном случае это "localhost:4000")
@@ -44,33 +65,10 @@ func main() {
 	// из флага, а не самим значением. Нам нужно убрать ссылку на указатель
 	// то есть перед использованием добавьте к нему префикс *. Обратите внимание, что мы используем
 	// функцию log.Printf() для записи логов в журнал работы нашего приложения.
-	log.Printf("Запуск сервера на http://127.0.0.1%s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
-} // stop server - Ctr+C
 
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-		}
-	}
-
-	return f, nil
+	// Применяем созданные логгеры к нашему приложению.
+	infoLog.Printf("Запуск сервера на http://127.0.0.1%s", *addr)
+	// Вызываем метод ListenAndServe() от нашей новой структуры http.Server
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
